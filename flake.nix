9815e9c082a4e742cb1f0c79e688ac38cd306445
Danny de Jong <ddejong@therp.nl>
2025-09-24 16:08:42 +0200

    Latest changes

--- flake.nix
+++ flake.nix
@@ -28,7 +28,8 @@
 
           ${pkgs.qemu_kvm}/bin/qemu-system-x86_64 -enable-kvm -boot d \
             -m 4G -cpu host -smp 2 -hda ~/vm.img \
-            -netdev user,id=net0,hostfwd=tcp::2222-:22 -device virtio-net-pci,netdev=net0
+            -netdev user,id=net0,hostfwd=tcp::2222-:22,hostfwd=tcp::8069-:8069,hostfwd=tcp::8072-:8072 \
+            -device virtio-net-pci,netdev=net0
         '';
 
         runShell = pkgs.writers.writeBashBin "run-shell" ''
@@ -53,24 +54,32 @@
               libtinfo-dev libncurses5-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev \
               libncursesw5-dev tk-dev libxmlsec1-dev libffi-dev liblzma-dev adduser lsb-base libxml2-dev \
               libxslt1-dev libpq-dev libsasl2-dev libopenjp2-7-dev \
-              libtiff5-dev libfreetype6-dev liblcms2-dev libwebp-dev openssh-server nano pipx pre-commit \
-              python3-dev rsync wget
-            pipx install "python-lsp-server[all]"
+              libtiff5-dev libfreetype6-dev liblcms2-dev libwebp-dev openssh-server nano \
+              postgresql pre-commit python3-dev ripgrep rsync wget
+            pip install "python-lsp-server[all]"
+            pip install pylint-odoo
             if [ ! -d /nix ]; then
               curl -L https://nixos.org/nix/install | sh -s -- --daemon --yes
             fi
+
+            sudo -u postgres psql -c "CREATE ROLE therp WITH LOGIN SUPERUSER"
+            sudo -u therp createdb therp
+            service postgresql restart
 
             echo "alias vi=nvim" >> /home/therp/.profile
             echo "alias g=git" >> /home/therp/.profile
             mkdir -p /home/therp/.config/nvim
             echo "luafile /etc/xdg/nvim/init.lua" > /home/therp/.config/nvim/init.vim
+            echo "local all all trust" > /var/lib/postgresql/14/main/pg_hba.conf
+            echo "host all all 127.0.0.1/32 trust" >> /var/lib/postgresql/14/main/pg_hba.conf
+            echo "host all all ::1/128 trust" >> /var/lib/postgresql/14/main/pg_hba.conf
 
             mkdir -p /root/.ssh
             mkdir -p /home/therp/.ssh
             chown therp:therp /home/therp/.ssh
           HEREDOC
           ssh -p 2222 root@localhost <<HEREDOC
-            nix-env -iA nixpkgs.neovim
+            nix-env -iA nixpkgs.neovim nixpkgs.rustup
           HEREDOC
           ssh -p 2222 therp@localhost <<HEREDOC
             git config --global user.name "Danny de Jong"
@@ -81,9 +90,12 @@
         shellVm = pkgs.writers.writeBashBin "shell" ''
           set -e
 
-          scp -P 2222 /etc/gitconfig root@localhost:/etc/gitconfig
-          scp -r -P 2222 /etc/xdg/nvim root@localhost:/etc/xdg/nvim
-          #rsync e "ssh -p 2222" /etc/xdg/nvim root@localhost:2222:/etc/xdg
+          rsync -IL -e "ssh -p 2222" /etc/gitconfig root@localhost:/etc/gitconfig
+          rsync -IL -e "ssh -p 2222" ~/.config/pylintrc therp@localhost:~/.config/pylintrc
+          rsync -IL -e "ssh -p 2222" ~/.pydocstyle.ini therp@localhost:~/.pydocstyle.ini
+          rsync -IL -e "ssh -p 2222" ~/code/odools.toml therp@localhost:~/odools.toml
+          rsync -ILr --del -e "ssh -p 2222" /etc/xdg/nvim root@localhost:/etc/xdg
+          rsync -ILr --del -e "ssh -p 2222" ~/.config/nvim therp@localhost:~/.config
           ssh -A -p 2222 therp@localhost
         '';
       in {
